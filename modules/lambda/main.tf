066dda9af6ca2398df9a6f03ddb20dd9156d9b61
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2024-03-20 15:47:22 +0200

    change lambda name to get-all-courses

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -2,15 +2,15 @@
 data "archive_file" "get_all_courses" {
     type = "zip"
 
-    source_file  = "modules/lambda/functions/get_all_courses/get_all_courses.js" 
-    output_path  = "modules/lambda/functions/get_all_courses/get_all_courses.zip" 
+    source_file  = "modules/lambda/functions/get-all-courses/get-all-courses.js" 
+    output_path  = "modules/lambda/functions/get-all-courses/get-all-courses.zip" 
 }
 
 
 resource "aws_lambda_function" "get_all_courses" {
  filename         = data.archive_file.get_all_courses.output_path
- function_name   = "get_all_courses"
+ function_name   = "get-all-courses"
  role            =  var.get_all_courses_arn
- handler         = "get_all_courses.handler"
+ handler         = "get-all-courses.handler"
  runtime         = "nodejs16.x"
 }
